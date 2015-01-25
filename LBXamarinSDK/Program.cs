604044efbd55984cb2ada5d60a9e8cb9d6a52117
Yehuda <yehuda.babayof@mail.huji.ac.il>
2015-01-25 19:30:40 +0200

    Initial push.

---
+++ LBXamarinSDK/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using LBXamarinSDK;
+using LBXamarinSDK.LBRepo;
+using Microsoft.CSharp;
+using Newtonsoft.Json.Linq;
+using RestSharp.Portable;
+
+/*
+namespace LBXamarinSDKGenerator
+{
+    public class Startup
+    {
+        public async Task<object> Invoke(object input)
+        {
+            string jsonModel = ((Object[]) input)[0].ToString();
+            
+            //System.IO.StreamWriter file = new System.IO.StreamWriter("D:\\debug.txt");
+            //file.Write(jsonModel);
+            //file.Close();
+            
+
+            string outputPath = ((Object[])input)[1].ToString();
+
+            var dynamicModelsTemplate = new DynamicModels();
+            var dynamicReposTemplate = new DynamicRepos();
+            var constantCode = new HardcodedModels();
+
+            dynamicModelsTemplate.Session = new Dictionary<string, object>();
+            dynamicModelsTemplate.Session["jsonModel"] = jsonModel;
+
+            dynamicReposTemplate.Session = new Dictionary<string, object>();
+            dynamicReposTemplate.Session["jsonModel"] = jsonModel;
+
+            dynamicModelsTemplate.Initialize();
+            dynamicReposTemplate.Initialize();
+            
+            string code = constantCode.TransformText() + dynamicReposTemplate.TransformText() +
+                          dynamicModelsTemplate.TransformText();
+
+            CompilerParameters compilerParams = new CompilerParameters() { OutputAssembly = outputPath };
+
+            compilerParams.ReferencedAssemblies.Add("System.dll");
+            compilerParams.ReferencedAssemblies.Add("System.Core.dll");
+            compilerParams.ReferencedAssemblies.Add("System.Linq.dll");
+            compilerParams.ReferencedAssemblies.Add("System.Net.Http.dll");
+            compilerParams.ReferencedAssemblies.Add("System.Runtime.dll");
+            compilerParams.ReferencedAssemblies.Add("/RestSharp.Portable.dll");
+            compilerParams.ReferencedAssemblies.Add("/Newtonsoft.Json.dll");
+
+            compilerParams.WarningLevel = 1;
+            compilerParams.TreatWarningsAsErrors = false;
+
+            var compiler = new CSharpCodeProvider();
+            var result = compiler.CompileAssemblyFromSource(compilerParams, code);
+
+            if (result.Errors.Count == 0)
+            {
+                Console.WriteLine("Loopback Xamarin SDK DLL compiled successfully.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Loopback Xamarin SDK DLL Compilation errors:");
+                foreach (var err in result.Errors)
+                {
+                    Console.WriteLine(err.ToString());
+                }
+                return false;
+            }
+            
+           
+            
+        }
+    }
+}
+
+*/
+
+namespace LBXamarinSDK
+{
+    public class DebugProgram
+    {
+        private static void Main(string[] args)
+        {
+            Gateway.SetServerBaseURL(new Uri("http://10.0.0.27:3000/api/"));
+            Gateway.SetDebugMode(true);
+
+            Trainer h = new Trainer()
+            {
+                email = "g@gmail.com",address = "fefe"
+            };
+            
+            Console.ReadKey();
+        }
+    }
+}
+    
---
+++ lb-xm/README.md
@@ -0,0 +1,13 @@
+# loopback-sdk-angular-cli
+
+**NOTE: The loopback-sdk-angular-cli module supersedes [loopback-angular-cli](https://www.npmjs.org/loopback-angular-cli). Please update your package.json accordingly.**
+
+CLI tools for the [LoopBack AngularJS SDK](https://github.com/strongloop/loopback-sdk-angular).
+
+See the official [LoopBack AngularJS SDK documentation](http://docs.strongloop.com/display/DOC/AngularJS+JavaScript+SDK)
+for more information.
+
+## Mailing List
+
+Discuss features and ask questions on [LoopBack Forum](https://groups.google.com/forum/#!forum/loopbackjs).
+
